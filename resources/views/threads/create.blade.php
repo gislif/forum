827cdf2b2f0885513b5e32b4f10e69fd124cc375
gisli2 <gislif88@gmail.com>
2017-09-07 14:24:24 +0000

    Heimaverkefni 1 gagn

---
+++ resources/views/threads/create.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Create a New Thread</div>
+
+                <div class="panel-body">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Title:</label>
+                        <input type="title" class="form-control" id="exampleInputEmail1" placeholder="Title">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Body:</label>
+                        <div></div>
+                        <textarea rows="6" cols="100"></textarea>
+                        <div></div>
+                        <button type="submit" class="btn btn-primary">Publish</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
