5e10bd652b0525980f47bddf3c87979d58b8ee20
gisli2 <gislif88@gmail.com>
2017-09-12 14:43:55 +0000

    Timaverk 5

---
+++ resources/views/threads/greeting.blade.php
@@ -0,0 +1,8 @@
+<html>
+	<body>
+		<h1>
+			This is greeting number {{ $id }} with
+			{{ $count }} messages.
+		</h1>
+	</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,11 @@ Route::get('/threads', function() {
 Route::get('/threads/create', function () {
 	return view('threads.create');
 });
+
+Route::get('/greeting/{id}', function ($id) {
+return view('threads.greeting', ['id' => $id]);
+});
+
+Route::get('/greeting/{id}/message/{count}', function ($id, $count) {
+return view('threads.greeting', compact('id', 'count'));
+});
