c5c2037ba546c26db4fa8fd2e8a7ff58ad99382d
gisli2 <gislif88@gmail.com>
2017-09-13 15:02:24 +0000

    Timaverk 6 gagn tv

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/greeting/{id}', function ($id) {
 	$greetings = ['Sjáumst', 'Ciao' , 'Aloha', 'Hola', 'ello bub'];
 	#return $greetings[$id];
 	return view('threads.greeting', ['viewBreyta' => $greetings[$id]]);
-});
+})->where('id', '[0-4]');
 
 #Route::get('/greeting/{id}/message/{count}', function ($id, $count) {
 #	return view('threads.greeting', compact('id', 'count'));
