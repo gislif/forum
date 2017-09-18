74ee9edd411811a144970200f0c53b840a65b0be
gisli2 <gislif88@gmail.com>
2017-09-18 15:18:46 +0000

    New Stuff Ye

---
+++ app/Http/Controllers/ThreadsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+class ThreadsController extends Controller
+{
+    public function index()
+    {
+        $threads = DB::select('select * from threads');
+
+    	return view('threads.index', compact('threads'));
+    }
+    public function create()
+    {
+        $countries = ['Ísland', 'Ísland', 'Írland', 'Ísland', 'Írland'];
+    	return view('threads.create', compact('countries'));
+    }
+    public function show($id)
+    {
+    	$thread = DB::select('select * from threads where id=?');
+
+
+    	return view('threads.show');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,7 @@
 
 Auth::routes();
 
-Route::get('/threads', function() {
-	return view('threads.index');
-});
 
-Route::get('/threads/create', function () {
-	return view('threads.create');
-});
-
-Route::get('/greeting/{id}', function ($id) {
-	$greetings = ['Sjáumst', 'Ciao' , 'Aloha', 'Hola', 'ello bub'];
-	#return $greetings[$id];
-	return view('threads.greeting', ['viewBreyta' => $greetings[$id]]);
-})->where('id', '[0-4]');
-
-#Route::get('/greeting/{id}/message/{count}', function ($id, $count) {
-#	return view('threads.greeting', compact('id', 'count'));
-#});
+Route::get('/threads', 'ThreadsController@index');
+Route::get('/threads/create', 'ThreadsController@create');
+Route::get('/threads/{id}', 'ThreadsController@show');
